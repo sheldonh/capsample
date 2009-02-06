8434b82fedb8b8697a2c482a71d7653d2980a29a
Sheldon Hearn <sheldonh@starjuice.net>
2009-02-06 20:57:03 +0200

    Initial import of virgin rails app.

---
+++ app/controllers/application.rb
@@ -0,0 +1,15 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => 'a4a98a3194e4e7ef5203dfbcf78dfd85'
+  
+  # See ActionController::Base for details 
+  # Uncomment this to filter the contents of submitted sensitive data parameters
+  # from your application log (in this case, all fields with names like "password"). 
+  # filter_parameter_logging :password
+end
