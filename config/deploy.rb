3ac56e7dd1586ee40d45a9b20ec70556c9158b2d
Sheldon Hearn <sheldonh@starjuice.net>
2009-02-07 00:13:39 +0200

    Share sqlite3 databases across releases.
    * Abuses capistrano parallel() helper (repeated calls).
    * Relies on sqlite3 to follow dangling symlinks.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,5 @@
 set :application, "capsample"
+set :rails_env, "development"
 set :repository,  "git://github.com/sheldonh/capsample.git"
 set :deploy_via, :remote_cache # Does a git pull instead of a full repo fetch
 
@@ -18,11 +19,34 @@ role :db,  "lenny.vmware", :primary => true
 #set :user, "sheldonh"
 set :use_sudo, false
 
-desc "Creates empty directories not tracked by git"
-task :before_finalize_update, :roles => :web do
-  [ File.join("public", "stylesheets") ].each do |empty_dir|
-    full_path = File.join(current_release, empty_dir)
-    run "mkdir -p #{current_release}/#{empty_dir}"
+set :shared_db_dir, File.join(deploy_to, shared_dir, "db")
+
+desc "Create shared db directory for sqlite3."
+task :after_setup, :roles => [ :db ] do
+  run "mkdir -p #{shared_db_dir}"
+end
+
+desc "Handle directories not tracked by git, and shared sqlite3 databases."
+task :before_finalize_update, :roles => [ :web, :db ] do
+  [ File.join("public", "stylesheets") ].each do |missing_dir|
+    full_path = File.join(current_release, missing_dir)
+    parallel do |session|
+      session.when "in?(:web)", "mkdir -p #{current_release}/#{missing_dir}"
+    end
+  end
+
+  [ File.join(current_release, "db") ].each do |missing_dir|
+    parallel do |session|
+      session.when "in?(:db)", "mkdir -p #{missing_dir}"
+    end
+  end
+
+  [ "development", "test", "production" ].each do |db|
+    link_name = File.join(current_release, "db", "#{db}.sqlite3")
+    link_to = File.join(shared_db_dir, "#{db}.sqlite3")
+    parallel do |session|
+      session.when "in?(:db)", "ln -s #{link_to} #{link_name}"
+    end
   end
 end
 
