a1a0f917d739a70d314e74f29d64591e8fe05aa1
Sheldon Hearn <sheldonh@starjuice.net>
2009-02-07 00:57:05 +0200

    Support passenger restart method.

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,37 +16,47 @@ role :app, "lenny.vmware"
 role :web, "lenny.vmware"
 role :db,  "lenny.vmware", :primary => true
 
+set :app_server, :passenger
+
 #set :user, "sheldonh"
 set :use_sudo, false
 
 set :shared_db_dir, File.join(deploy_to, shared_dir, "db")
 
-desc "Create shared db directory for sqlite3."
-task :after_setup, :roles => [ :db ] do
-  run "mkdir -p #{shared_db_dir}"
+namespace :deploy do
+  desc "Create shared db directory for sqlite3."
+  task :after_setup, :roles => :db do
+    run "mkdir -p #{shared_db_dir}"
+  end
+
+  desc "Handle directories not tracked by git, and shared sqlite3 databases."
+  task :before_finalize_update, :roles => [ :web, :db ] do
+    [ File.join("public", "stylesheets") ].each do |missing_dir|
+      full_path = File.join(current_release, missing_dir)
+      parallel do |session|
+        session.when "in?(:web)", "mkdir -p #{current_release}/#{missing_dir}"
+      end
+    end
+
+    [ File.join(current_release, "db") ].each do |missing_dir|
+      parallel do |session|
+        session.when "in?(:db)", "mkdir -p #{missing_dir}"
+      end
+    end
+
+    [ "development", "test", "production" ].each do |db|
+      link_name = File.join(current_release, "db", "#{db}.sqlite3")
+      link_to = File.join(shared_db_dir, "#{db}.sqlite3")
+      parallel do |session|
+        session.when "in?(:db)", "ln -s #{link_to} #{link_name}"
+      end
+    end
+  end
+
+  if [ :passenger ].include?(app_server)
+    desc "Requests a Phusion Passenger restart."
+    task :restart, :roles => :app, :except => { :no_release => true } do
+      run "touch #{current_path}/tmp/restart.txt"
+    end
+  end
 end
-
-desc "Handle directories not tracked by git, and shared sqlite3 databases."
-task :before_finalize_update, :roles => [ :web, :db ] do
-  [ File.join("public", "stylesheets") ].each do |missing_dir|
-    full_path = File.join(current_release, missing_dir)
-    parallel do |session|
-      session.when "in?(:web)", "mkdir -p #{current_release}/#{missing_dir}"
-    end
-  end
-
-  [ File.join(current_release, "db") ].each do |missing_dir|
-    parallel do |session|
-      session.when "in?(:db)", "mkdir -p #{missing_dir}"
-    end
-  end
-
-  [ "development", "test", "production" ].each do |db|
-    link_name = File.join(current_release, "db", "#{db}.sqlite3")
-    link_to = File.join(shared_db_dir, "#{db}.sqlite3")
-    parallel do |session|
-      session.when "in?(:db)", "ln -s #{link_to} #{link_name}"
-    end
-  end
-end
-
