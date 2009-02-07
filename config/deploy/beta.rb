335a262d9446298b67c4986dc2f401c108b0e7e9
Sheldon Hearn <sheldonh@starjuice.net>
2009-02-07 23:12:28 +0200

    Pulled in multistage support from edge.

---
+++ config/deploy/beta.rb
@@ -0,0 +1,4 @@
+set :stage, "beta"
+set :branch, "edge"
+set :rails_env, "development"
+set :deploy_to, "/var/www/apps/#{application}-beta"
---
+++ config/deploy/production.rb
@@ -0,0 +1,4 @@
+set :stage, "production"
+set :branch, "master"
+set :rails_env, "production"
+set :deploy_to, "/var/www/apps/#{application}"
